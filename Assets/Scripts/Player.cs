afbdd47218c4ffa268a6b8e279edd1c72fc7bd32
Soel Antonio Attala <soelattala7@gmail.com>
2022-07-09 00:50:19 -0300

    Preparamos la escena y los materiales a utilizar

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    public float hor;
+    public float ver;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void MovimientoJugador()
+    {
+        
+    }
+}
