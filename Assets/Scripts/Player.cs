056a45f6411781390bb43921f52e875d92686775
Soel Antonio Attala <soelattala7@gmail.com>
2022-07-09 04:30:11 -0300

    modificamos vectores para variar los movimientos de los cubos

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour
     public float speed = 5f;
     public float hor;
     public float ver;
+    public float rotateSpeed;
+    
+
 
     void Start()
     {
@@ -16,11 +19,18 @@ public class Player : MonoBehaviour
     
     void Update()
     {
-        
+        MovimientoJugador();
+
     }
+
 
     void MovimientoJugador()
     {
-        
+        float ver = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(0, 0, ver) * speed * Time.deltaTime);
+
+        float hor = Input.GetAxis("Horizontal");
+        transform.Rotate(new Vector3(0, hor, 0) * rotateSpeed * Time.deltaTime);
     }
+
 }
